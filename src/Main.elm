bb0241ebf1b4878fe2664d545c86cd739cc8fcee
Ralf Northman <ralf.northman@bredband.net>
2020-08-24 10:10:02 +0200

    Comments, type signatures etc.

--- src/Main.elm
+++ src/Main.elm
@@ -6,25 +6,26 @@ import Color
 import Direction2d
 import Element exposing (Element)
 import Element.Background as Background
-import Element.Border as Border
 import Element.Font as Font
 import Element.Input as Input
 import FlatColors.IndianPalette as Palette
-import Frame2d
+import Frame2d exposing (Frame2d)
 import Geometry.Svg as Svg
 import Html exposing (Html)
 import Pixels exposing (Pixels, inPixels, pixels)
 import Point2d
-import Point3d
-import Polygon2d
 import Quantity exposing (Quantity)
 import Rectangle2d
 import Svg exposing (Svg)
 import TypedSvg
 import TypedSvg.Attributes
 import TypedSvg.Attributes.InPx
-import TypedSvg.Types exposing (..)
-import Vector2d
+import TypedSvg.Types exposing (CoordinateSystem(..), Paint(..))
+import Vector2d exposing (Vector2d)
+
+
+
+---- MODEL ----
 
 
 type alias Model =
@@ -38,6 +39,10 @@ init =
     { lengthA = pixels 40.0
     , lengthB = pixels 14.0
     }
+
+
+
+---- UPDATE ----
 
 
 type Msg
@@ -59,12 +64,16 @@ update msg model =
             }
 
 
+
+---- VIEW ----
+
+
 view : Model -> Html Msg
 view model =
     Element.layout [] <|
         Element.column
-            [ Element.spacing 20
-            , Element.padding 10
+            [ Element.spacing 30
+            , Element.padding 30
             , Element.centerX
             ]
             [ mySlider
@@ -81,8 +90,12 @@ view model =
                 , min = 6
                 , max = 60
                 }
-            , Element.el [] (Element.html <| myPattern model)
+            , Element.el [] <| Element.html <| myPattern model
             ]
+
+
+
+---- Colors ----
 
 
 grey : Element.Color
@@ -108,6 +121,10 @@ colorB =
     Color.rgb255 red green blue
 
 
+
+---- Slider ----
+
+
 type alias MySliderInput =
     { label : String
     , input : Quantity Float Pixels
@@ -126,7 +143,6 @@ mySlider input =
                 , Element.height <| Element.px 2
                 , Element.centerY
                 , Background.color grey
-                , Border.rounded 2
                 ]
                 Element.none
         ]
@@ -140,13 +156,31 @@ mySlider input =
         }
 
 
+
+---- Pythagorean tiling ----
+
+
+type YUpCoordinates
+    = YUpCoordinates
+
+
+type YDownCoordinates
+    = YDownCoordinates
+
+
+type TopLeftCorner
+    = TopLeftCorner
+
+
 myPattern : Model -> Html Msg
 myPattern model =
     let
+        sceneWidth : Quantity number Pixels
         sceneWidth =
-            500
+            pixels 500
 
-        square side color =
+        mySquare : Quantity Float Pixels -> Color.Color -> Svg msg
+        mySquare side color =
             Svg.rectangle2d
                 [ TypedSvg.Attributes.fill <| Paint color
                 , TypedSvg.Attributes.stroke <| Paint Color.black
@@ -155,26 +189,34 @@ myPattern model =
             <|
                 Rectangle2d.from Point2d.origin (Point2d.xy side side)
 
-        -- The names of these -ish-vectors only makes sense when lengthA is significantly bigger than lengthB
+        -- The names of these -ish-vectors only makes sense when lengthA is significantly longer than lengthB (like A > 3B).
+        rightish : Vector2d Pixels YUpCoordinates
         rightish =
             Vector2d.xy model.lengthA model.lengthB
 
+        leftish : Vector2d Pixels YUpCoordinates
         leftish =
             rightish |> Vector2d.reverse
 
+        upish : Vector2d Pixels YUpCoordinates
         upish =
             Vector2d.perpendicularTo rightish
 
+        squareA : Svg msg
         squareA =
-            square model.lengthA colorA
+            mySquare model.lengthA colorA
 
+        squareB : Svg msg
         squareB =
-            square model.lengthB colorB
+            mySquare model.lengthB colorB
                 |> Svg.translateBy downB
 
+        downB : Vector2d Pixels YUpCoordinates
         downB =
             Vector2d.withLength model.lengthB Direction2d.negativeY
 
+        -- This is all the geometry needed to fill up a svg "pattern window."
+        neededSquares : Svg msg
         neededSquares =
             TypedSvg.g []
                 [ squareA
@@ -192,17 +234,22 @@ myPattern model =
                     |> Svg.translateBy rightish
                 ]
 
+        -- For clockwise rotation of our neededSquares so they can be seamlessly tiled by svg-pattern (we want neededSquares to line up with the "pattern window" as in the left red square in the picture at https://en.wikipedia.org/wiki/Pythagorean_tiling#Pythagorean_theorem_and_dissections).
+        negativeAngle : Angle
         negativeAngle =
             Angle.atan2
                 (Quantity.negate model.lengthB)
                 model.lengthA
 
+        -- Side length of the "pattern window." Pythagorean theorem in the guise of the elm-units package.
+        hypotenuse : Quantity Float Pixels
         hypotenuse =
             Quantity.sqrt <|
                 Quantity.plus
                     (Quantity.squared model.lengthA)
                     (Quantity.squared model.lengthB)
 
+        defs : Svg msg
         defs =
             TypedSvg.defs []
                 [ TypedSvg.pattern
@@ -219,38 +266,36 @@ myPattern model =
                     ]
                 ]
 
+        tiling : Svg msg
         tiling =
             Svg.rectangle2d
                 [ TypedSvg.Attributes.fill <| Reference "Pattern" ]
             <|
                 Rectangle2d.from Point2d.origin
-                    (Point2d.pixels sceneWidth sceneWidth)
+                    (Point2d.xy sceneWidth sceneWidth)
 
+        elements : Svg msg
         elements =
-            TypedSvg.g []
-                [ defs
-                , tiling
-                , neededSquares
-                    |> Svg.translateBy rightish
-                    |> Svg.translateBy rightish
-                    |> Svg.translateBy rightish
-                    |> Svg.translateBy rightish
-                    |> Svg.translateBy rightish
-                    |> Svg.translateBy upish
-                ]
+            TypedSvg.g [] [ defs, tiling ]
 
+        topLeftFrame : Frame2d Pixels YDownCoordinates { defines : TopLeftCorner }
         topLeftFrame =
-            Frame2d.atPoint (Point2d.pixels 0 sceneWidth)
+            Frame2d.atPoint (Point2d.xy (pixels 0) sceneWidth)
                 |> Frame2d.reverseY
 
+        scene : Svg msg
         scene =
-            Svg.relativeTo topLeftFrame elements
+            elements |> Svg.relativeTo topLeftFrame
     in
     Svg.svg
-        [ TypedSvg.Attributes.InPx.width sceneWidth
-        , TypedSvg.Attributes.InPx.height sceneWidth
+        [ TypedSvg.Attributes.InPx.width (inPixels sceneWidth)
+        , TypedSvg.Attributes.InPx.height (inPixels sceneWidth)
         ]
         [ scene ]
+
+
+
+---- Main ----
 
 
 main : Program () Model Msg
